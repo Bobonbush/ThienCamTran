9426208ea6fa93c536ae4ce845bd6891fbda52dc
Bobonbush <tdadung23@apcs.fitus.edu.vn>
2026-06-21 00:59:15 +0700

    Test commit

--- Assets/Scripts/Tile/Slidable.cs
+++ Assets/Scripts/Tile/Slidable.cs
@@ -20,7 +20,6 @@ public class Sliable : MonoBehaviour
 
     private IEnumerator PassThroughRoutine(Collider2D playerCollider)
     {
-        // Nothing
         Physics2D.IgnoreCollision(playerCollider, tilemapCollider, true);
 
 
